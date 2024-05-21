d45ec955700160de6474ffd62be5eccce99c3717
Agnieszka <agamjp@gmail.com>
2024-05-22 01:10:15 +0200

    minor corrections

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+import os
 import requests
 from datetime import date, timedelta
 from send_email import send_email
+from dotenv import load_dotenv
 
+load_dotenv()
 today = date.today()
 d = timedelta(days=7)
 seven_days_ago = today - d
 
+topic = input("Enter the topic that interests you:")
+
 print(seven_days_ago)
-api_key = "fcb94f8c82844455aefb8cb11f48af26"
-url = "https://newsapi.org/v2/everything?q=data+engineering&from=" \
-       + str(seven_days_ago) + "&sortBy=publishedAt&apiKey=" + api_key \
-      + "&language=en"
+api_key = os.getenv("API_KEY")
+url = f"https://newsapi.org/v2/everything?q={topic}" \
+      f"&from={seven_days_ago}&sortBy=publishedAt&apiKey={api_key}&language=en"
 
 request = requests.get(url)
 content = request.json()
@@ -24,7 +28,7 @@ with open("digest.txt", "w", encoding="utf-8") as file:
 with open("digest.txt", "r", encoding="utf-8") as file_:
     raw_message = file_.read()
 
-subject = "Your last week's Data Engineering digest!"
+subject = f"Your last week's {topic.title()} digest!"
 
 send_email(raw_message, subject)
 
